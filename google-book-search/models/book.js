da2efc5ffa092e9493af051f86c51e43084781a7
patel <parthpatel167@gmail.com>
2021-02-23 17:37:56 -0600

    changes in book models

--- google-book-search/models/book.js
+++ google-book-search/models/book.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  authors: { type: String, required: true },
+  description: { type: String, default: '' },
+  image: { type: String, default: 'www.cnn.com' },
+  link: { type: String, default: '' },
 });
-
-const Book = mongoose.model("Book", bookSchema);
+const Book = mongoose.model('Book', bookSchema);
 
 module.exports = Book;
