dafe5de5b75ef0f01b711a33143e2d3b86dddbbc
patel <parthpatel167@gmail.com>
2021-02-25 11:01:33 -0600

    deploying

--- google-book-search/server.js
+++ google-book-search/server.js
@@ -1,20 +1,24 @@
 const express = require('express');
-
+const app = express();
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const morgan = require('morgan');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan('tiny'));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 // Add routes, both API and view
 app.use(routes);
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/reactreadinglist',
