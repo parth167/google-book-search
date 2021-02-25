2683b7d57d0598b80ef200e9be1da64f47673e0c
patel <parthpatel167@gmail.com>
2021-02-25 09:56:31 -0600

    nav bar link works

--- google-book-search/client/src/components/Nav/index.js
+++ google-book-search/client/src/components/Nav/index.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <Link className="navbar-brand" to="/Books">
-        Google Book Search
-      </Link>
-
-      <Link className="navbar-brand" to="/Detail">
-        Go to Saved Books
-      </Link>
-
-      {/* </a> */}
+      <div className="container-fluid">
+        <h1 className="navbar-brand">Google Books</h1>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Search
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/saved">
+                Saved
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
